229b02923c9bed59aa2845625fc1902b6e2aaada
Vanh020104 <114908216+Vanh020104@users.noreply.github.com>
2024-01-29 15:32:34 +0700

    code exam

---
+++ ExamAsp/Data/ExamAspContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ExamAsp.Models;
+
+namespace ExamAsp.Data
+{
+    public class ExamAspContext : DbContext
+    {
+        public ExamAspContext (DbContextOptions<ExamAspContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ExamAsp.Models.Department> Department { get; set; } = default!;
+
+        public DbSet<ExamAsp.Models.Employee> Employee { get; set; } = default!;
+    }
+}
---
+++ ExamAsp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExamAsp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
